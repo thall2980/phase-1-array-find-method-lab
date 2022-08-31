1eff547bc5ca8911aadc4e183d68258483376c65
Tyler Hall <thall2980@gmail.com>
2022-08-30 23:40:34 -0400

    commit tree 2bf4ad14fabef903be80811b87dde648ad0736f0

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+// // code your solution here
+
+// con
+
+// const record = array.find(({result}) => === 'W');
+//     if (win){
+//         return element.year
+//     }
+
+// function superbowlWin(record) {
+//     if (record.result === "W"){
+//         return record.year
+// }
+
+
+//function superbowlWin(record) 
+
+superbowlWin = (record) => {
+    const result = record.find(element => element.result === "W")
+    return result ? (result.year) : undefined
+}
+//     if (record.result === "W"){
+//         return record.year
+//     }
+// }
